818e47b720ae09504343f76b66601333f3c273c4
Marcelo Colazo <mcolazo@gmail.com>
2020-09-17 06:11:58 -0300

    Ejercicio 1.2

--- practico1.py
+++ practico1.py
@@ -2,10 +2,31 @@ import numpy as np
 
 
 def ev_facturas(nfacturas=0):
+    '''
+    Dado una número de facturas, implementar una función que devuelva el 
+    string `Cantidad de facturas: <nro>` donde `nro` es el número que se 
+    pasa como argumento.
+    '''
     if nfacturas <= 12:
         print('Cantidad de facturas: {}'.format(nfacturas))
     else:
         print('Cantidad de facturas: muchas')
 
 
-test = ev_facturas(-5)
+def ambos(s=''):
+    '''
+    Dado un string `s`, implementar la función `ambos` que devuelve un string
+    construido con los dos primeros y dos últimos caracteres.
+    '''
+    if len(s)< 2:
+        res = ''
+    else:
+        res = s[0] + s[1] + s[-2] + s[-1]
+    return res
+
+
+ev_facturas(-5)
+print(ambos('primavera'))
+
+
+
